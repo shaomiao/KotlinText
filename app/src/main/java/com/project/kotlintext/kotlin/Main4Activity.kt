2932d35d282758863f9f19abcd48f9f48a2d04a1
shaomiao <shaomiaojava@163.com>
2017-06-09 16:57:03 +0800

    提交

--- app/src/main/java/com/project/kotlintext/kotlin/Main4Activity.kt
+++ app/src/main/java/com/project/kotlintext/kotlin/Main4Activity.kt
@@ -3,20 +3,21 @@ package com.project.kotlintext.kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.project.kotlintext.R
+import java.io.File
 
 /**
  * 最新
  */
 class Main4Activity : AppCompatActivity() {
 
-    fun describe(obj:Any):String =
-    when(obj) {
-        1 -> "One"
-        "Hello" -> "Greeting"
-        is Long -> "Long"
-        !is String -> "Not a string"
-        else -> "Unknown"
-    }
+    fun describe(obj: Any): String =
+            when (obj) {
+                1 -> "One"
+                "Hello" -> "Greeting"
+                is Long -> "Long"
+                !is String -> "Not a string"
+                else -> "Unknown"
+            }
 
     // 习惯用语
     //创建DTOs(POJOs/POCOs)数据类
@@ -24,21 +25,101 @@ class Main4Activity : AppCompatActivity() {
      * 为所有属性添加getters如果为var类型同时添加setters -- equals()
      * --haseCode()--toString()--copy()--commponent1()
      */
-    data class Customer(val name:String,val email:String)
+    data class Customer(val name: String, val email: String)
 
     // 函数默认值
-    fun foo(a: Int = 0,b: String = "") {}
+    fun foo(a: Int = 0, b: String = "") {}
 
-    val list = listOf(3,2,0)
+    val list = listOf(3, 2, 0)
     // 过滤list
-    val positives1 = list.filter{x -> x >0 }
-    val positives = list.filter{it >0 }
+    val positives1 = list.filter { x -> x > 0 }
+    val positives = list.filter { it > 0 }
+
+    fun String.spcaceToCamelCase() {
+//        println("nihao")
+    }
+    object Resource {
+        val name = "Name"
+    }
+    val files = File("Test").listFiles()
+
+    fun transform(color:String): Int {
+        return when(color) {
+            "Red" -> 0
+            "Green" -> 1
+            "Blue" -> 2
+            else -> throw IllegalArgumentException("Invalid color param ")
+        }
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main4)
-        println(positives)
+//        setContentView(R.layout.activity_main4)
+
+        println(transform("Green"))
+
+        // 如果不为空执行某操作
+//        val data = ""
+//        data?.let {
+//            //
+//        }
+
+        // 声明为空执行某操作
+//        val data = mapOf("email" to null )
+//        val email = data["email"]?:throw IllegalStateException("Email is missing");
+        //如果不为空... 否则...的简写
+//        println(files?.size?:"empty")
+//        println("shaomiao".spcaceToCamelCase());
+        // 只读list
+//        val list = listOf("a","b","c")
+//
+//        // 只读map
+//        val map = mapOf("a" to 1,"b" to 2,"c" to 3)
+//        println(map["a"])
+        // 使用ranges
+        // 如果x 在1到10之间
+//        var x = 11;
+//        if (x in 1..10) {
+//            println("hello world")
+//        }
+        // 从10到1
+//        for (x in 10 downTo 1) {
+//            print("$x ,")
+//        }
+        // 从2开始 没2个一步
+//        for (x in 2..10 step 2) {
+//            print("$x ,")
+//        }
+        // 半开区间 不包括 100
+//        for (i in 1 until 100) {
+//            print("$i ,")
+//        }
+        // 闭区间 包括100
+//        for (i in 1..100) {
+//            print("$i,")
+//        }
+        println("shaomiao")
+
+        // 实例检查
+//        when (x) {
+//            is Foo ->
+//                ...
+//            is Bar ->
+//                ...
+//            else ->
+//                ...
+//        }
+        // 遍历map/list
+//        for ((k,v) in map) {
+//            print("$k -> $v")
+//        }
+
+        // 字符串插值
+//        var name = "shaomiao";
+//        println("Name $name")
+
+//        println(positives)
 //        println(describe(1))
 //        println(describe("Hello"))
 //        println(describe(1000L))
@@ -96,8 +177,6 @@ class Main4Activity : AppCompatActivity() {
 //                .sortedBy { it }
 //                .map { it.toUpperCase() }
 //                .forEach { println(it) }
-
-
 
 
     }
